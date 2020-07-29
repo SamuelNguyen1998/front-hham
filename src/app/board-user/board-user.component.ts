08ff22fe94ea3344a2017f564163369247a87187
NhatTu <nhattu199892@gmail.com>
2020-07-29 23:37:39 +0700

    Update view voted

--- src/app/board-user/board-user.component.ts
+++ src/app/board-user/board-user.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { UserService } from '../_services/user.service';
+//import { UserService } from '../_services/user.service';
+import { ActivityService } from '../_services/activity.service';
+
+import { FlashMessagesModule } from 'angular2-flash-messages';
 
 @Component({
   selector: 'app-board-user',
@@ -8,19 +11,43 @@ import { UserService } from '../_services/user.service';
 })
 export class BoardUserComponent implements OnInit {
 
-  content: string;
+  // content: string;
 
-  constructor(private userService: UserService) { }
+  // constructor(private userService: UserService) { }
 
-  ngOnInit(): void {
-    this.userService.getUserBoard().subscribe(
-      data => {
-        this.content = data;
-      },
-      err => {
-        this.content = JSON.parse(err.error).message;
-      }
-    );
+  // ngOnInit(): void {
+  //   this.userService.getUserBoard().subscribe(
+  //     data => {
+  //       this.content = data;
+  //     },
+  //     err => {
+  //       this.content = JSON.parse(err.error).message;
+  //     }
+  //   );
+  // }
+
+  @Input() activity: Activity;
+  selected: number;
+  votingEnded = false;
+
+  constructor(private activityService: ActivityService, private flashMessagesService: FlashMessagesService) { }
+
+  ngOnInit() {
+    const now = new Date();
+
+    if (new Date(this.activity.endDate).getTime() < now.getTime()) {
+      this.votingEnded = true;
+    }
+  }
+
+  vote() {
+    this.activityService.vote(this.activity.id, this.selected).subscribe(success => {
+      this.activity.voted = true;
+      this.flashMessagesService.show('Vote submitted!', { cssClass: 'card-panel green lighten-4', timeout: 3000 });
+    }, error => {
+      console.log(error);
+      this.flashMessagesService.show('You can\'t vote twice', { cssClass: 'card-panel red lighten-3', timeout: 3000 });
+    });
   }
 
 }
