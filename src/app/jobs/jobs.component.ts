76f5a764d192c6489016326bf923382375c38d22
nqvuong99qn <vuong.nguyen.quang@tpssoft.com>
2020-08-03 12:44:38 +0700

    add  jobs

---
+++ src/app/jobs/jobs.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { MatTableModule} from '@angular/material/table';
+
+
+
+@Component({
+  selector: 'app-jobs',
+  templateUrl: './jobs.component.html',
+  styleUrls: ['./jobs.component.scss']
+})
+export class JobsComponent implements OnInit {
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
