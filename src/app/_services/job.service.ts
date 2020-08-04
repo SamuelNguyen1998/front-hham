9f966bf199e5e43933cf1de27545e1ae9a04d04d
nqvuong99qn <vuong.nguyen.quang@tpssoft.com>
2020-08-04 14:23:21 +0700

    Update job.service.ts

--- src/app/_services/job.service.ts
+++ src/app/_services/job.service.ts
@@ -11,7 +11,27 @@ export class JobService {
 
   constructor(private http: HttpClient) { }
 
+  getAll(): Observable<any> {
+    return this.http.get(`${Constants.API_BASE}/jobs`);
+  }
+
+  get(id): Observable<any> {
+    return this.http.get(`${Constants.API_BASE}/jobs/${id}`);
+  }
+
+  create(data): Observable<any>{
+    return this.http.post(`${Constants.API_BASE}/jobs`, data);
+  }
+
+  update(id, data): Observable<any> {
+    return this.http.put(`${Constants.API_BASE}/jobs/${id}`, data);
+  }
+
+  delete(id): Observable<any> {
+    return this.http.delete(`${Constants.API_BASE}/jobs/${id}`);
+  }
+
   findByName(name): Observable<any> {
-    return this.http.get(`${Constants.API_BASE}/projects?name=${name}`);
+    return this.http.get(`${Constants.API_BASE}/jobs?name=${name}`);
   }
 }
