ab132ef9aabb4276c02d7d51bd4945237495526e
nqvuong99qn <vuong.nguyen.quang@tpssoft.com>
2020-08-04 12:01:06 +0700

    Update jobs.component.ts

--- src/app/jobs/jobs.component.ts
+++ src/app/jobs/jobs.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { MatTableModule} from '@angular/material/table';
-
+import { Job} from '../_models/Job';
+import { JobService} from '../_services/job.service';
+import { AuthService } from '../_services/auth.service';
 
 
 @Component({
@@ -9,11 +11,20 @@ import { MatTableModule} from '@angular/material/table';
   styleUrls: ['./jobs.component.scss']
 })
 export class JobsComponent implements OnInit {
+  jobs: Job[];
+  name: string;
 
-
-  constructor() { }
+  constructor(public auth: AuthService, 
+              private jobService: JobService) { }
 
   ngOnInit(): void {
+  }
+
+  searchByName(): void {
+    this.jobService.findByName(this.name).subscribe(
+      response => this.jobs = response.data,
+      error => console.log(error)
+    );
   }
 
 }
