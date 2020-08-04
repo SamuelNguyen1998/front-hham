448bacd58059114d0740b187f53d61c8866afb08
nqvuong99qn <vuong.nguyen.quang@tpssoft.com>
2020-08-04 16:32:57 +0700

    Create fakedata_jobs.ts

---
+++ src/app/jobs/fakedata_jobs.ts
@@ -0,0 +1,9 @@
+import { Job} from '../_models/Job';
+
+export const JOBS: Job[] = [
+    {id: 1, name: 'CEO', amountMoney: 300000},
+    {id: 2, name: 'Senior Manager', amountMoney: 200000},
+    {id: 3, name: 'Manager', amountMoney: 150000},
+    {id: 4, name: 'Project Manager', amountMoney: 100000},
+    {id: 5, name: 'Developer', amountMoney: 50000}
+  ];
